160279ce967ff3c47433847cd91b88b0ea7c8991
Ivan Masmitjà <imasmitja@gmail.com>
2015-11-08 19:53:14 +0100

    Prism application template

---
+++ PrismApplication/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace $safeprojectname$
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Bootstrapper bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
---
+++ PrismApplication/Bootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Modularity;
+using Prism.Unity;
+using System.Windows;
+
+namespace $safeprojectname$
+{
+    internal class Bootstrapper: UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            // Modules defined in code
+            return new ModuleCatalog();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            // Add your modules here
+        }
+
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+
+            // Register your types in container here            
+        }
+    }
+}
