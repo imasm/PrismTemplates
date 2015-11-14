6440e0350c9ab180497c060a3131ea2df74eba63
Ivan Masmitjà <imasmitja@gmail.com>
2015-11-14 15:26:13 +0100

    Module Template

---
+++ PrismModule/Module.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using Microsoft.Practices.Unity;
+
+namespace $safeprojectname$
+{
+	public class Module: IModule
+    {
+        private readonly IUnityContainer theContainer;
+        private readonly IRegionManager theRegionManager;
+
+        public Module(IUnityContainer container, IRegionManager regionManager)
+        {
+            theContainer = container;
+            theRegionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            // Register types
+            // ex: theContainer.RegisterInstance<interface, type>();
+
+            // Register views
+            // ex: theRegionManager.Regions.RegisterViewWithRegion(regions.Name, typeof(MyView));
+        }
+    }
+}
